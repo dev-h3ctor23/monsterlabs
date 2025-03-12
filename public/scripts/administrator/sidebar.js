17832380dae8d6ca41f3717a49a033f4059e1f96
MiryamHu <158163620+MiryamHu@users.noreply.github.com>
2025-03-12 01:16:33 +0100

    ultimos detallitos

--- public/scripts/administrator/sidebar.js
+++ public/scripts/administrator/sidebar.js
@@ -1,3 +1,20 @@
+
+// ---------------------CODIGO PARA CUANDO SE VUELE ATRAS AL CERRAR SESION-----------------------º
+// Empuja un estado al historial
+window.history.pushState(null, null, window.location.href);
+
+// Listener para el evento popstate (cuando se presiona "atrás")
+window.addEventListener('popstate', function (event) {
+window.location.replace("../../../mvc/views/log-in.html");
+});
+
+// Listener para el evento pageshow (para detectar carga desde la cache)
+window.addEventListener('pageshow', function (event) {
+  if (event.persisted) {
+    window.location.replace("../../../mvc/views/log-in.html");
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     fetch('../../components/sidebar-administrator.html')
         .then(response => response.text())
@@ -18,3 +35,22 @@ function setupTabSwitching() {
         });
     });
 }
+
+// --------------------------CERRAR SESION----------------------------
+document.addEventListener("click", function (event) {
+    // Detectar si se hizo clic en el botón de salir
+    let logoutBtn = event.target.closest("#logoutBtn");
+    if (logoutBtn) {
+        event.preventDefault();
+  
+        fetch("../../mvc/controllers/logout.php")
+            .then(response => response.json()) // Parsear la respuesta como JSON
+            .then(data => {
+                if (data.redirect) {
+                    // Redirigir al usuario si la respuesta indica una redirección
+                    window.location.href = data.redirect;
+                }
+            })
+            .catch(error => console.error("Error al cerrar sesión:", error));
+    }
+  });
